afc120854a52cf894a6572f2145828bc13c7a581
ekowal@earnestresearch.com <emottk@gmail.com>
2017-09-29 17:40:30 -0400

    client specification

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ require('dotenv').config()
 var Twitter = require('twitter');
 var amazon = require('amazon-product-api');
 
-var client = amazon.createClient({
+var amazon_client = amazon.createClient({
   awsId: process.env.AWS_ID,
   awsSecret: process.env.AWS_SECRET,
 });
 
-var client = new Twitter({
+var twitter_client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-client.post('statuses/update', {status: 'trrrrt'},  function(error, tweet, response) {
+twitter_client.post('statuses/update', {status: 'testt'},  function(error, tweet, response) {
   if(error) throw error;
-  console.log(tweet);  // Tweet body.
-  console.log(response);  // Raw response object.
+  console.log(tweet);
+  console.log(response);
 });
 
-//
-// client.itemSearch({
-//   director: 'Quentin Tarantino',
-//   actor: 'Samuel L. Jackson',
-//   searchIndex: 'DVD',
-//   audienceRating: 'R',
-//   responseGroup: 'ItemAttributes,Offers,Images'
-// }).then(function(results){
-//   console.log(results);
-// }).catch(function(err){
-//   console.log(err);
-// });
+
+amazon_client.itemSearch({
+  director: 'Quentin Tarantino',
+  actor: 'Samuel L. Jackson',
+  searchIndex: 'DVD',
+  audienceRating: 'R',
+  responseGroup: 'ItemAttributes,Offers,Images'
+}).then(function(results){
+  console.log(results);
+}).catch(function(err){
+  console.log(err);
+});
